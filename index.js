6f48b7755eabdb31b1e79f326c8eab890896e598
JenelicaGathoni <168233892+JenelicaGathoni@users.noreply.github.com>
2024-05-05 11:57:03 +0300

    Add files via upload
    adding a blackjack game

--- index.js
+++ index.js
@@ -1,31 +1,71 @@
-let saveElOne = document.getElementById("save__el__one")
-let saveElTwo = document.getElementById("save__el__two")
+let cards = []
+let sum = 0 ;
+let hasBlackJack = false;
+let isAlive = false;
+let message =""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.querySelector("#sum-el");
+let cardsEl = document.querySelector("#cards-el")
 
-let countOne = document.getElementById("count__one")
-let countTwo =document.getElementById("count__two")
 
-let count = 0
-let sum = 0
-let addition = 0
-function increment(){
-    count += 1
-    countOne.textContent  = count
-}
-function manage () {
-    let countStr = count + " - "
-    saveElOne.textContent += countStr
-    countOne.textContent = 0
-    count = 0
-}
-function add(){
-    sum += 1
-    countTwo.textContent  = sum
-}
-function save () {
-    let sumStr = sum + " - "
-    saveElTwo.textContent += sumStr
-    countTwo.textContent = 0
-    sum = 0
+let player  = {
+    name:"Jenelle",
+    chips: 145
 }
 
 
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name +": $" + player.chips;
+
+//let sumEl = document.getElementById("sum-el") 
+function getRandomCard(){
+   
+    let randomNumber = Math.floor( Math.random() *13) + 1;
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1){
+        return 11
+    } else{
+        return randomNumber
+    }
+}
+
+
+function newCard (){
+    if ( isAlive === true && hasBlackJack === false){
+    let card = getRandomCard ();
+    sum += card;
+    cards.push(card)
+    renderGame()
+    }
+}
+function renderGame (){
+if (sum <= 20) {
+    message = "Do you want to draw a new card ";
+    
+}
+else if(sum === 21) {
+    message =  " You've got blackjack " ;
+    hasBlackJack = true;
+}
+else {
+    message = "You are out of the game! ";
+    isAlive = false;
+}
+
+// CASH OUT
+messageEl.textContent = message
+sumEl.textContent = "sum: "  + sum ;
+cardsEl.textContent = "cards: "
+for (i =0; i < cards.length; i++){
+    cardsEl.textContent += cards[i] +" "
+}
+}
+function startGame (){
+    isAlive = true;
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    cards = [firstCard,secondCard];
+    sum = firstCard + secondCard;
+    renderGame()
+}
